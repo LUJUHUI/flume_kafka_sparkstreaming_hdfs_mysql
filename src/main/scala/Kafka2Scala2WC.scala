9429c068a7a5f8a2c4cba4cdb78c824152370215
LUJUHUI <brucelujuhui@163.com>
2018-08-01 09:33:50 +0800

    update

--- src/main/scala/Kafka2Scala2WC.scala
+++ src/main/scala/Kafka2Scala2WC.scala
@@ -14,21 +14,31 @@ object Kafka2Scala2WC {
     val conf = new SparkConf().setAppName("Kafka2Scala2WC").setMaster("local[*]")
     val ssc = new StreamingContext(conf, Seconds(5))
     val kafkaParams = Map[String, Object](
+      /*kafka的端口号*/
       "bootstrap.servers" -> "manager:9092,namenode:9092,datanode:9092",
+      /*k-v的反序列化*/
       "key.deserializer" -> classOf[StringDeserializer],
       "value.deserializer" -> classOf[StringDeserializer],
+      /*kafka的组号*/
       "group.id" -> "kafka_wc",
+      /*偏移量重置*/
       "auto.offset.reset" -> "latest",
+      /*是否自动提交*/
       "enable.auto.commit" -> (false: java.lang.Boolean)
     )
 
+    /*kafka的已经创建的主题*/
     val topics = Array("realtime")
+
+    /*创建一个离散流*/
     val data = KafkaUtils.createDirectStream[String, String](
       ssc,
       PreferConsistent,
       Subscribe[String, String](topics, kafkaParams)
     )
+
     val lines = data.map(_.value())
+    /*对kafka消费端取出来的数据进行扁平化处理（string，string）-> string*/
     val words = lines.flatMap(_.split(" "))
     val wordAndOne = words.map((_, 1))
     val reduced = wordAndOne.reduceByKey(_ + _)
